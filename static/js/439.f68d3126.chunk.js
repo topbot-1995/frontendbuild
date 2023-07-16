cdc713b76713a5e87a3e2c08d00fe7cabef6bbd7
Topbot <topbot0319@gmail.com>
2023-07-16 12:40:45 +0300

    fix mobile wallet connect

---
+++ static/js/439.f68d3126.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunktradio=self.webpackChunktradio||[]).push([[439],{46601:function(){},24654:function(){},52361:function(){},94616:function(){}}]);
--- static/js/6.fad27ea9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptradio=this.webpackJsonptradio||[]).push([[6],{1111:function(i,o){}}]);
-//# sourceMappingURL=6.fad27ea9.chunk.js.map
